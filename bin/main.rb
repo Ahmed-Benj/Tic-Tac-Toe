f169061c1bb89e14c7fb071b05bc0ed1765a8ce6
Ahmed-Benj <ahmedblaugrana@gmail.com>
2021-04-23 08:03:57 +0100

    build welcome text

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+
+
+puts "Welcome to rubys Tic-Tac-Toe !\n\n"
+
+
+puts "Enter Player1 name:"
+player1_name = gets.chomp
+puts "Enter Player2 name:"
+player2_name = gets.chomp
+
+puts "\n#{player1_name} will play with X and #{player2_name} will play with O\n\n"
+puts "Let's start!\n\n"
