3da5b29eb8c01e325731113be41f2c2a1e0acc36
Ahmed-Benj <ahmedblaugrana@gmail.com>
2021-04-24 13:40:47 +0100

    add UI instructions for players

--- bin/main.rb
+++ bin/main.rb
@@ -11,3 +11,15 @@ player2_name = gets.chomp
 
 puts "\n#{player1_name} will play with X and #{player2_name} will play with O\n\n"
 puts "Let's start!\n\n"
+
+puts separator="+---+---+---+"
+puts row="| 1 | 2 | 3 |"
+puts separator
+puts row="| 4 | 5 | 6 |"
+puts separator
+puts row="| 7 | 8 | 9 |"
+puts separator + "\n\n"
+
+puts "It's #{player1_name}'s turn!\n\n"
+puts "Please select an available cell from the board"
+player1_name_choice1= gets
