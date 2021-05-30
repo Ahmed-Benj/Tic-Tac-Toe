8a8bf9d2640d52a8eba7aaaf64f43f24e9b443e5
ahmed BEN-JMII <ahmed.ben-jmii@talan.com>
2021-05-30 13:29:51 +0100

    add board class in lib folder

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 #rubocop:disable all
 
-board = ['', '', '', '', '', '', '', '', '']
-require_relative '../lib/tic_tac_toe.rb'
+require_relative '../lib/tic_tac_toe'
 
 def display_board(board)
   separator = '+---+---+---+'
@@ -12,7 +11,7 @@ def display_board(board)
   puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
   puts separator
   puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
-  puts separator + "\n\n"
+  puts "#{separator}\n\n"
 end
 
 def exit_game(reason)
@@ -57,39 +56,40 @@ player2 = Player.new('Player 2')
 
 puts "\n#{player1.name} will play with X and #{player2.name} will play with O\n\n"
 puts "Let's start!\n\n"
-display_board(board)
+board = Board.new
+display_board(board.board)
 
-while full(board) == false
+while board.full == false
   # query players for moves
   player1_move = querydata("It's #{player1.name}'s turn!\n\nPlease select an available cell from the board (1-9)")
-  while position_taken(board, player1_move.to_i - 1)
+  while board.position_taken(player1_move.to_i - 1)
     puts 'this position is taken, please choose another one'
-    player1_move = querydata("")
+    player1_move = querydata('')
   end
-  board = update_board(board, 'X', player1_move.to_i - 1)
-  display_board(board)
+  board.update_board('X', player1_move.to_i - 1)
+  display_board(board.board)
 
   # check if anyone has won ==> If yes break declaring the winner of the game
-  if won(board, 'X')
+  if board.won('X')
     puts "Congratulations ! #{player1.name} (X) has won the game !!"
     break
     # check if full(board) ==> If yes break declaring tie
-  elsif full(board)
+  elsif board.full
     puts 'Break Tie'
     break
   end
 
   player2_move = querydata("It's #{player2.name}'s turn!\n\nPlease select an available cell from the board (1-9)")
-  while position_taken(board, player2_move.to_i - 1)
+  while board.position_taken(player2_move.to_i - 1)
     puts 'this position is taken, please choose another one'
-    player2_move = querydata("")
+    player2_move = querydata('')
   end
-  board = update_board(board, 'O', player2_move.to_i - 1)
-  display_board(board)
-  if won(board, 'O')
+  board.update_board('O', player2_move.to_i - 1)
+  display_board(board.board)
+  if board.won('O')
     puts "Congratulations ! #{player2.name} (O) has won the game !!"
     break
-  elsif full(board)
+  elsif board.full
     puts 'Break Tie'
     break
   end
--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,35 +1,43 @@
 #rubocop:disable all
 
-WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+class Board
+  attr_accessor :board
 
-def won(board, symbol)
-  WIN_COMBINATIONS.each do |win_combination|
-    board_index0 = board[win_combination[0]]
-    board_index1 = board[win_combination[1]]
-    board_index2 = board[win_combination[2]]
-    return true if board_index0 == symbol && board_index1 == symbol && board_index2 == symbol
+  def initialize
+    @board = ['', '', '', '', '', '', '', '', '']
   end
-  false
-end
 
-def full(board)
-  board.all? { |e| e != '' }
-end
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ].freeze
 
-def position_taken(board, position)
-  board[position] != ''
-end
+  def won(symbol)
+    WIN_COMBINATIONS.each do |win_combination|
+      board_index0 = @board[win_combination[0]]
+      board_index1 = @board[win_combination[1]]
+      board_index2 = @board[win_combination[2]]
+      return true if board_index0 == symbol && board_index1 == symbol && board_index2 == symbol
+    end
+    false
+  end
 
-def update_board(board, symbol, position)
-  board = board.map.with_index { |x, i| i == position ? symbol : x }
-  board
+  def full
+    @board.all? { |e| e != '' }
+  end
+
+  def position_taken(position)
+    @board[position] != ''
+  end
+
+  def update_board(symbol, position)
+    @board = @board.map.with_index { |x, i| i == position ? symbol : x }
+    @board
+  end
 end
