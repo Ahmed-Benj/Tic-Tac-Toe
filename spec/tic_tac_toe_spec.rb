5e807da27f21b68ce24d5d5088a184e1673174e4
ahmed BEN-JMII <ahmed.ben-jmii@talan.com>
2021-06-11 21:57:22 +0100

    test won_draw method

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/player'
 require_relative '../lib/game'
 
 describe Game do
+
   let(:game) { Game.new }
 
   describe '#welcome_player' do
@@ -10,12 +11,23 @@ describe Game do
       expect(game.welcome_player('A', 'X')).to eq("\nA will play with X\n\n")
     end
   end
-  let(:board) { Board.new }
+
   describe '#display_board' do
     it 'displays board' do
       expect(game.display_board(board.board)).to eq("+---+---+---+\n|\
    |   |   |\n+---+---+---+\n|   |   |   |\n+---+---+---+\n|   |\
    |   |\n+---+---+---+\n\n")
+    end
+  end
+
+  describe '#won_draw' do
+    
+  let(:playerx) { Player.new('Player 1', 'X') }
+    #playerx.name = 'Player X'
+    let(:board) { Board.new }
+    board.board = ['X','O','X','X','O','O','O','X','X']
+        it 'defines winning or drawing' do
+      expect(game.won_draw(['X','O','X','X','O','O','O','X','X'],playerx)).to eq(false)
     end
   end
 end
